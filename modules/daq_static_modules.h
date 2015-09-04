4909fd4c759bd88b236a83c12a9ef743de638217
Michael Altizer <xiche@verizon.net>
2015-09-04 15:37:55 -0400

    modules: Fix static modules includes for DAQ_Module_t definition

--- modules/daq_static_modules.h
+++ modules/daq_static_modules.h
@@ -19,7 +19,7 @@
 ** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-#include <daq.h>
+#include <daq_api.h>
 
 #ifdef BUILD_AFPACKET_MODULE
 extern const DAQ_Module_t afpacket_daq_module_data;
