7e6657c1a013398349036ec52a222d4be4244de0
Michael Altizer <mialtize@cisco.com>
2016-09-13 14:02:59 -0400

    Switch to my newer, less dead e-mail address.

--- api/daq_common.h
+++ api/daq_common.h
@@ -1,6 +1,6 @@
 /*
 ** Copyright (C) 2010-2013 Sourcefire, Inc.
-** Author: Michael R. Altizer <maltizer@sourcefire.com>
+** Author: Michael R. Altizer <mialtize@cisco.com>
 **
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License Version 2 as
