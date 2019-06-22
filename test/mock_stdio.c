6aa35d589e533a7788107412c394026ad45954db
Michael Altizer <mialtize@cisco.com>
2019-06-22 02:50:08 -0400

    copyright: Do the 2019 copyright dance

--- test/mock_stdio.c
+++ test/mock_stdio.c
@@ -1,5 +1,5 @@
 /*
-** Copyright (C) 2018 Cisco and/or its affiliates. All rights reserved.
+** Copyright (C) 2018-2019 Cisco and/or its affiliates. All rights reserved.
 ** Author: Michael R. Altizer <mialtize@cisco.com>
 **
 ** This program is free software; you can redistribute it and/or modify
