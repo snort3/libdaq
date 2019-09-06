45aefa58eaeee9c208a7075c7c52b7e58f46a958
5u623l20 <5u623l20@users.noreply.github.com>
2019-09-06 17:29:29 +0600

    Fix build on FreeBSD

--- api/daq_common.h
+++ api/daq_common.h
@@ -30,6 +30,9 @@ extern "C" {
 #include <sys/time.h>
 #include <stdint.h>
 #include <unistd.h>
+#if defined(__OpenBSD__) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__DragonFly__)
+#include <sys/socket.h>
+#endif
 
 #ifndef DAQ_SO_PUBLIC
 #  ifdef HAVE_VISIBILITY
