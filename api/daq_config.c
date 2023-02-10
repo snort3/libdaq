23afcfa77a98b9f4090b3b8a8e7877908f9ef595
Steven Baigal (sbaigal) <sbaigal@cisco.com>
2023-02-10 01:45:08 +0000

    Pull request #57: copyright: update for year 2023
    Merge in SNORT/libdaq from ~RSHAFIQ/libdaq:copyright_change_year_2023 to master
    
    Squashed commit of the following:
    
    commit b30e12ce68e814f8d198f279acb031a9d71fbc8c
    Author: rshafiq <rshafiq@cisco.com>
    Date:   Mon Feb 6 15:02:22 2023 -0500
    
        copyright: update for year 2023

--- api/daq_config.c
+++ api/daq_config.c
@@ -1,5 +1,5 @@
 /*
-** Copyright (C) 2015-2022 Cisco and/or its affiliates. All rights reserved.
+** Copyright (C) 2015-2023 Cisco and/or its affiliates. All rights reserved.
 ** Author: Michael R. Altizer <mialtize@cisco.com>
 **
 ** This program is free software; you can redistribute it and/or modify
